a3e1b41349c8288c103982f68aa820ad45aba1d9
Ralph <rlphwkm@gmail.com>
2024-10-23 22:52:40 -0400

    fixing audio issues

--- pages/aboutme/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const backgroundMusic = document.getElementById('background-music');
-    backgroundMusic.volume = 0.25; // Adjust volume if necessary
-    backgroundMusic.play().catch(error => {
-        console.error("Error playing background music:", error);
-    });
-});
